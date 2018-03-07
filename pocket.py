9635df2ce5f72fb73edeee51ea1591aaff006fe7
MANOJ RAJALBANDI <manojrajalbandi@MANOJs-MacBook-Pro.local>
2018-03-07 11:01:17 -0800

    Completed Pocket

--- pocket.py
+++ pocket.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import random
 
-alpha = 0.1
+alpha = 0.001
 mina = 2000
 minw = []
 def isValid(prod1,label, rows, w) :
@@ -25,20 +25,21 @@ def isValid(prod1,label, rows, w) :
 			minw = w
 		ind = random.randrange(0,len(a))
 		return a[ind]
+	else:
+		mina = 0
+		minw = w
 	return -1
 
 def pocket(data, label, rows):
 	a = []
-	for i in range(0,3) :
+	for i in range(0,4) :
 		#a.append(random.uniform(-1,1))
-		a.append(0)
+		a.append(4.0)
 	w = np.array([a])
 	count = 0
-	#print w.T
-	#print data.T
 	prod = np.dot(w, data.T)
 	index = isValid(prod, label, rows, w)
-	while (index != -1) and count < 100:
+	while (index != -1) and count < 7000:
 		term = np.dot(w, data[index].T)
 		if (label[index] == 1) and prod[0][index] < 0:
 			while term[0] < 0:
@@ -51,16 +52,19 @@ def pocket(data, label, rows):
 		count  = count + 1
 		prod = np.dot(w, data.T)
 		index = isValid(prod, label, rows, w)
+	print "The no. of violated terms for best solution:"
+	print  mina
+	print "The best possible weights:"
+	print minw
+	print "The iterations taken: " + str(count)
 	return
 
 if __name__ == '__main__':
 	df = pd.read_csv('classification.txt', sep = ",", header = None)
 	rows = df[0].count()
 	df1 = df.iloc[0:rows,0:3]
-	df2 = pd.DataFrame(1, index = range(rows), columns = range(1))
-	frames = [df2,df1]
-	df3 = pd.concat(frames)
-	#print df1
-	labels = df[3]
-	#print labels
-	pocket(df1.as_matrix(), labels, rows)
+	labels = df[4]
+	features = df1.as_matrix()
+	intercept = np.ones((features.shape[0],1))
+	features = np.hstack((intercept,features))
+	pocket(features, labels, rows)
